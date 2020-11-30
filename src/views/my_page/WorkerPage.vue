b187dbefa9ea3d490ebd4fd4cfb3b55f484e922c
blurzky <843481949@qq.com>
2020-11-30 23:13:14 +0800

    合并

--- src/views/my_page/WorkerPage.vue
+++ src/views/my_page/WorkerPage.vue
@@ -25,7 +25,7 @@
       <van-cell v-if="menus.includes(4)" class="cell" title="缴费上报" icon="shopping-cart-o" @click="$router.push('/mall?home=11')" />
       <van-cell v-if="menus.includes(5)" class="cell" title="课程打分" icon="good-job-o" @click="$router.push(`/comment?role=1}`)" />
       <van-cell v-if="menus.includes(6)" class="cell" title="教练评语" icon="comment-o" @click="$router.push(`/comment?role=${userinfo.role}`)" />
-      <van-cel v-if="menus.includes(7)" class="cell" title="我的二维码" icon="scan" @click="$router.push(`/qr`)" />
+      <van-cell v-if="menus.includes(7)" class="cell" title="我的二维码" icon="scan" @click="$router.push(`/qr`)" />
       <van-cell v-if="menus.includes(8)" class="cell" title="我的二维码" icon="scan" @click="$router.push(`/my_qr`)" />
     </div>
   </div>
